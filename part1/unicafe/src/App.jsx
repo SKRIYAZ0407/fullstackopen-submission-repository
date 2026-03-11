2f29d902247fc15c65f642a0db3acd25687eeffd
SHAIK.RIYAZ <skriyaz0407@gmail.com>
2026-03-11 23:13:06 +0530

    statistics are formatted as tables

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,7 +8,7 @@ const Button = (props) => {
 
 const StatisticLine  = (props) => {
   return(
-    <p>{props.text} {props.value}</p>
+    <td>{props.text} {props.value}</td>
   )
 }
 const Statistics  = (props) => {
@@ -20,13 +20,16 @@ const Statistics  = (props) => {
   return (
     <div>
     <h2>statistics</h2>
-      <StatisticLine  text="good" value = {props.good}/>
-      <StatisticLine  text="neutral" value = {props.neutral}/>
-      <StatisticLine  text="bad" value = {props.bad}/>
-      <StatisticLine  text="all" value = {props.good + props.neutral + props.bad}/>
-      <StatisticLine  text="average" value = {(props.good -props.bad)/(props.good + props.neutral + props.bad)} />
-      <StatisticLine  text ="postive feedback" value = {(props.good * 100)/(props.good + props.neutral + props.bad) }/>
-      </div>
+    <table>
+      <tr><StatisticLine  text="good" value = {props.good}/></tr>
+      
+      <tr><StatisticLine  text="neutral" value = {props.neutral}/> </tr>
+      <tr><StatisticLine  text="bad" value = {props.bad}/></tr>
+      <tr><StatisticLine  text="all" value = {props.good + props.neutral + props.bad}/></tr>
+      <tr><StatisticLine  text="average" value = {(props.good -props.bad)/(props.good + props.neutral + props.bad)} /></tr>
+      <tr><StatisticLine  text ="postive feedback" value = {(props.good * 100)/(props.good + props.neutral + props.bad) }/></tr>
+    </table>
+    </div>
   );
 }
 
